45786608e20adfc083a3c35fe374794d3961e4d3
nxjoshi <45656214+nxjoshi@users.noreply.github.com>
2022-07-22 13:39:21 +0530

    Update diagonalDifference.go

--- diagonalDifference.go
+++ diagonalDifference.go
@@ -1,24 +1,14 @@
-package main
+
+	type Matrix [][]int32
+	oneMatrix := Matrix{{11, 2, 4}, {4, 10, 6}, {10, 8, 12}}
+	number package main
 
 import (
-	"bufio"
 	"fmt"
-	"io"
 	"math"
-	"os"
-	"strconv"
-	"strings"
 )
 
-/*
- * Complete the 'diagonalDifference' function below.
- *
- * The function is expected to return an INTEGER.
- * The function accepts 2D_INTEGER_ARRAY arr as parameter.
- */
-
 func diagonalDifference(arr [][]int32) int32 {
-	// Write your code here
 	var lsum, rsum int32
 	size := len(arr)
 
@@ -37,57 +27,6 @@ func diagonalDifference(arr [][]int32) int32 {
 
 }
 
-func main() {
-	reader := bufio.NewReaderSize(os.Stdin, 16*1024*1024)
-
-	stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
-	checkError(err)
-
-	defer stdout.Close()
-
-	writer := bufio.NewWriterSize(stdout, 16*1024*1024)
-
-	nTemp, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
-	checkError(err)
-	n := int32(nTemp)
-
-	var arr [][]int32
-	for i := 0; i < int(n); i++ {
-		arrRowTemp := strings.Split(strings.TrimRight(readLine(reader), " \t\r\n"), " ")
-
-		var arrRow []int32
-		for _, arrRowItem := range arrRowTemp {
-			arrItemTemp, err := strconv.ParseInt(arrRowItem, 10, 64)
-			checkError(err)
-			arrItem := int32(arrItemTemp)
-			arrRow = append(arrRow, arrItem)
-		}
-
-		if len(arrRow) != int(n) {
-			panic("Bad input")
-		}
-
-		arr = append(arr, arrRow)
-	}
-
-	result := diagonalDifference(arr)
-
-	fmt.Fprintf(writer, "%d\n", result)
-
-	writer.Flush()
-}
-
-func readLine(reader *bufio.Reader) string {
-	str, _, err := reader.ReadLine()
-	if err == io.EOF {
-		return ""
-	}
-
-	return strings.TrimRight(string(str), "\r\n")
-}
-
-func checkError(err error) {
-	if err != nil {
-		panic(err)
-	}
+func main() {:= diagonalDifference(oneMatrix)
+	fmt.Print(number)
 }
